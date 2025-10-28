c11def3f0be0a5335b57acc9cd31288560ba3478
Olivier Koch <olivier.koch@gmail.com>
2025-10-28 11:53:37 +0100

    added carry

---
+++ src/nn/modules/utils.py
@@ -0,0 +1,8 @@
+import torch
+
+def trunc_normal_init(shape, std=1.0):
+    """Truncated normal initialization."""
+    t = torch.randn(shape) * std
+    # Truncate to [-2*std, 2*std]
+    t = torch.clamp(t, -2*std, 2*std)
+    return t

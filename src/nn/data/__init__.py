7034880568ec9787dae70fe0db6ac16aefdff43a
Olivier Koch <olivier.koch@gmail.com>
2025-11-10 10:21:32 +0100

    flexible sudoku

--- src/nn/data/__init__.py
+++ src/nn/data/__init__.py
@@ -1,3 +1,3 @@
 from .arc_datamodule import ARCDataModuleWithPuzzles
-from .sudoku4x4_datamodule import Sudoku4x4DataModule
+from .sudoku4x4_datamodule import SudokuDataModule
 from .xor_datamodule import XORDataModule
